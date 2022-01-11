c38bb784413ba909d76fed2a01e550c62b44f775
Brendan Smith <brendansmith@Brendans-MacBook-Air.local>
2022-01-10 21:55:27 -0600

    added phones

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.Phones
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    """Return homepage."""
-    # change the original return statement you wrote to the one below
-    return render_template('home.html', msg='Flask is Cool!!')
+phones= db.phones
+
+@app.route('/',)
+def home():
+    '''return homepage'''
+    return render_template('home.html', phones=phones.find())
+
+@app.route('/iphone',)
+def iphone():
+    '''return homepage'''
+    return render_template('iphone.html', phones=phones.find())
+
+@app.route('/samsung',)
+def samsung():
+    '''return homepage'''
+    return render_template('samsung.html', phones=phones.find())
+
+@app.route('/google',)
+def google():
+    '''return homepage'''
+    return render_template('google.html', phones=phones.find())
